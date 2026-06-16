c52703923be3231a734c2db6c127c93dc2bcf305
inspirelabs2025 <Rogiervisser76@gmail.com>
2026-06-17 01:00:37 +0200

    feat(customer_success): Add chat nudge variant to tailwind config and update tsconfig to allow env variables access

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -195,7 +195,7 @@ export default {
 							fontWeight: '600',
 							color: 'inherit',
 						},
-					a: {
+						a: {
 							color: 'hsl(var(--primary))',
 							textDecoration: 'none',
 							'&:hover': {
